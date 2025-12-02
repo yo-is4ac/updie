0dc9d494f27370d6b1500980c03fab703de61e49
yo-is4ac <yo.isaac.romero@gmail.com>
2025-12-02 19:26:11 -0300

    feat/v1

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ import re
 import tkinter as tk
 from tkinter import ttk
 
-# Is folder accesible? if not, can i list the groups inside it? 
-# If so i will search for everyone 
-# If not takeOwn From the Dir
-
 def is_folder_accessible(target_folder):
     try:
         os.listdir(target_folder)
@@ -21,10 +17,10 @@ def is_list_group_available(target_folder):
         return False
 
 def take_own_dir(target_folder):
-    sub.run(["takeown", "/f", "/r", "/d", "y", target_folder], check=True, capture_output=True)
+    sub.run(["takeown", "/f", target_folder, "/r", "/d", "y"], check=True, capture_output=True)
 
 def is_everyone_present(target_folder):
-    pattern = r"\(Everyone\)"
+    pattern = r"Everyone"
     text = sub.run(["icacls", target_folder], capture_output=True, check=True, text=True)
 
     match_object = re.search(pattern, str(text))
@@ -46,8 +42,20 @@ def get_current_status(target_folder):
     
     if match_object:
         return match_object.group(2)
-    
-    return False
+
+def format_current_status(permission):
+    if permission == 'N':
+        return "Disabled"
+    elif permission == 'F':
+        return 'Enabled'
+
+def disableWindowsUpdate(target_folder, label):
+    sub.run(["icacls", target_folder, "/deny", "Everyone:F"], capture_output=True, check=True, text=True)
+    label.config(text="Windows Update Status: Disabled")
+
+def enableWindowsUpdate(target_folder, label):
+    sub.run(["icacls", target_folder, "/grant", "Everyone:F"], capture_output=True, check=True, text=True)
+    label.config(text="Windows Update Status: Enabled")
 
 def main():
     root = tk.Tk()
@@ -62,10 +70,18 @@ def main():
         
         if (is_everyone_present(target_folder) == False):
             add_everyone_to_group(target_folder)
-               
-        label = ttk.Label(master=root, text="Everyone Status: " + str(get_current_status(target_folder)))
+        
+        status = get_current_status(target_folder)
+        status_formatted = format_current_status(status)
+
+        label = ttk.Label(master=root, text=f"Windows Update Status: {status_formatted}")
         label.pack(padx=20)
 
+        button_disable = ttk.Button(master=root, text="Disable Windows Update", command=lambda: disableWindowsUpdate(target_folder, label))
+        button_enable = ttk.Button(master=root, text="Enable Windows Update", command=lambda: enableWindowsUpdate(target_folder, label))
+        
+        button_disable.pack(pady=30)
+        button_enable.pack(pady=30)
     except Exception as e:
         label = ttk.Label(master=root, text="path= " + target_folder + "\n" + str(e))
         label.pack(padx=20)
